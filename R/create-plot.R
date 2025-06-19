8870465b35cf4e6416e5dc4756552841f00848a5
MattB242Training <M.Bright2@liverpool.ac.uk>
2025-06-19 13:41:20 +0100

    mismatch blue change

--- R/create-plot.R
+++ R/create-plot.R
@@ -10,13 +10,13 @@ df = read_csv("data/weatherAUS.csv")
 
 # Create a scatter plot of Humidity9am and Temp9am in a location
 
-city = "Sydney"
+city = "Brisbane"
 
 scatter_plot = 
 df %>%
   filter(Location == city) %>%
   ggplot(aes(x = Temp9am, y = Humidity9am)) + 
-  geom_point(colour = "red")
+  geom_point(colour = "blue")
 
 # Save the plot in graphics/
 ggsave(filename = "graphics/scatter-temp-humidity.png", plot = scatter_plot)
